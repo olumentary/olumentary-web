92ac496d32c92266c1ff8bcbb62da4410b15d418
Kevin Custer <kevin@kevin-custer.com>
2024-04-24 18:53:28 -0400

    Tailwind config, use erb tokens in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,md,liquid,erb,serb,rb}', './frontend/javascript/**/*.js'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      primary: colors.indigo,
+      secondary: colors.zinc,
+      accent: colors.teal,
+      danger: colors.rose,
+      midnight: {
+        100: '#edf1fc',
+        700: '#222c42',
+        900: '#0f172a',
+      },
+    },
     extend: {
       backgroundImage: {
         'hero-background': "url('/images/hero.jpg')",
